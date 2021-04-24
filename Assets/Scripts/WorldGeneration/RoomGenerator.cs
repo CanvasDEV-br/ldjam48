60290caf2cee395ce0a9dbf8c6c22f9eb7416baf
CanvasDEV-br <75525719+CanvasDEV-br@users.noreply.github.com>
2021-04-24 17:14:44 -0300

    Procedural rooms system
    i do a prefab and scene to u test :)

--- Assets/Scripts/WorldGeneration/RoomGenerator.cs
+++ Assets/Scripts/WorldGeneration/RoomGenerator.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class RoomGenerator : MonoBehaviour
 {
     public GameObject initialRoom;
+    public GameObject bossRoom;
     public GameObject[] roomsPrefabs;
     public int quantityCenterRooms;
     public int quantityAlternativeRooms;
     public float roomWidth;
 
+    [Range((int)0, (int)100)]
+    public int chancesToNull = 20; //what's the chance to NO spawn?
+    [Range((int)0, (int)100)]
+    public int chancesToRight = 50; //what's the chance to spawn for right?
+    [Range((int)0, (int)100)]
+    public int bossSpawnInTopCenter = 50; //what's the chance to spawn the room of boss in top center in finish row?
+
     //if the initial rooms is in scene before starts, roomsInScene = 1, if it is generate in start roomsInScene = 0
-    private int totalRoomsInScene = 0;
+    private int totalCenterRoomsInScene = 0;
+    private GameObject currentRoom; //current Alternative room
+    private bool isRight; //if the last currentRoom is Right
 
     void Start()
     {
@@ -20,34 +30,62 @@ public class RoomGenerator : MonoBehaviour
 
     void Generate()
     {
-        totalRoomsInScene++;
-        int rightRooms = 0;
-        int leftRooms = 0;
+        GameObject temp = new GameObject();
 
-        GameObject initialPoint = Instantiate(initialRoom, Vector3.zero, Quaternion.identity);
+        int rightRooms = 1;
+        int leftRooms = 1;
+        totalCenterRoomsInScene++;
+
+        GameObject initialPoint = Instantiate(initialRoom, new Vector3(0, 0, 0), Quaternion.identity);
 
         for (int i = 0; i < quantityCenterRooms; i++)
         {
-            GameObject temp = Instantiate(roomsPrefabs[Random.Range(0, roomsPrefabs.Length)], new Vector3(0, initialPoint.transform.position.y + roomWidth * totalRoomsInScene, 0), Quaternion.identity);
+            temp = Instantiate(roomsPrefabs[Random.Range(0, roomsPrefabs.Length)], new Vector3(0, initialPoint.transform.position.y + roomWidth * totalCenterRoomsInScene, 0), Quaternion.identity);
 
-            for (int a = 0; a < quantityAlternativeRooms; i++)
+            //---Side Rooms of center rooms---
+            for (int j = 0; j < quantityAlternativeRooms; j++)
             {
                 int rand = Random.Range(0, 100);
 
-                if (rand < 99) //right
+                if(rand >= chancesToNull)
                 {
-                    SpawnPrefab(new Vector3(temp.transform.position.x + roomWidth * rightRooms, temp.transform.position.y, temp.transform.position.z));
+                    rand = Random.Range(0, 100);
 
-                    rightRooms++;
+                    if (rand <= chancesToRight) //right
+                    {
+                        SpawnPrefab(new Vector3(temp.transform.position.x + roomWidth * rightRooms, temp.transform.position.y, temp.transform.position.z));
+
+                        rightRooms++;
+                        isRight = true;
+                    }
+                    else //left
+                    {
+                        SpawnPrefab(new Vector3(temp.transform.position.x + (roomWidth * -leftRooms), temp.transform.position.y, temp.transform.position.z));
+
+                        leftRooms++;
+                        isRight = false;
+                    }
                 }
             }
 
-            totalRoomsInScene++;
+            rightRooms = 1;
+            leftRooms = 1;
+            totalCenterRoomsInScene++;
+        }
+
+        //bossRoom spawn in center
+        if (Random.Range(0, 100) <= bossSpawnInTopCenter)
+        {
+            Instantiate(bossRoom, new Vector3(0, temp.transform.position.y + roomWidth, temp.transform.position.z), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(bossRoom, new Vector3(currentRoom.transform.position.x, currentRoom.transform.position.y + roomWidth, currentRoom.transform.position.z), Quaternion.identity);
         }
     }
 
     void SpawnPrefab(Vector3 position)
     {
-        Instantiate(roomsPrefabs[Random.Range(0, roomsPrefabs.Length)], position, Quaternion.identity);
+       currentRoom = Instantiate(roomsPrefabs[Random.Range(0, roomsPrefabs.Length)], position, Quaternion.identity);
     }
 }
